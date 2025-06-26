5ed932658a62979dc6ce9bac1020d9aab7e88910
locknew <bangbang12353@gmail.com>
2025-06-26 14:45:09 +0700

    add port in app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -86,7 +86,7 @@ def get_available_months():
     except Exception as e:
         return jsonify({'message': str(e)}), 500
 
-
+port = int(os.environ.get('PORT', 8000))
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=port)
